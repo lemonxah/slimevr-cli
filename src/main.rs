0a8b83bc8b53c57e654105a4a0a47c726ce0f3ce
lemonxah <lemon.xah@gmail.com>
2024-10-21 23:32:04 +0200

    fixed reset

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::time::Duration;
+
+use log::{error, info};
 use solarxr_protocol::rpc::{
     ResetRequest, ResetRequestArgs, ResetType, RpcMessage, RpcMessageHeader, RpcMessageHeaderArgs,
 };
 use solarxr_protocol::{MessageBundle, MessageBundleArgs};
+use tungstenite::protocol::frame::coding::CloseCode;
+use tungstenite::protocol::CloseFrame;
 use tungstenite::{connect, Message};
 
 use clap::{Parser, Subcommand};
@@ -19,14 +24,18 @@ struct Cli {
 }
 
 fn main() {
+    env_logger::init();
+
     let cli = Cli::parse();
-    match cli.command {
+    if let Err(err) = match cli.command {
         Commands::FullReset => send_reset(ResetType::Full),
         Commands::YawReset => send_reset(ResetType::Yaw),
+    } {
+        println!("Error sending reset: {:?}", err);
     }
 }
 
-fn send_reset(rtype: ResetType) {
+fn send_reset(rtype: ResetType) -> Result<(), tungstenite::Error> {
     let mut fbb = flatbuffers::FlatBufferBuilder::new();
     let args = RpcMessageHeaderArgs {
         tx_id: None,
@@ -49,6 +58,33 @@ fn send_reset(rtype: ResetType) {
 
     fbb.finish(message, None);
     let data = fbb.finished_data().to_vec();
-    let (mut socket, _resposne) = connect("ws://localhost:21110").unwrap();
-    socket.send(Message::Binary(data)).unwrap();
+
+    let (mut socket, _response) = connect("ws://localhost:21110").unwrap();
+    if let Err(err) = socket.send(Message::Binary(data)) {
+        error!("Error sending message: {:?}", err);
+    }
+
+    loop {
+        if let Ok(msg) = socket.read() {
+            if let Ok(message) = msg.to_text() {
+                if message.is_empty() {
+                    info!("empty message");
+                    break;
+                }
+                info!("Received: {}", message);
+            } else {
+                info!("done");
+                break;
+            }
+        } else {
+            info!("unable to read socket");
+            break;
+        }
+    }
+
+    let close_frame = CloseFrame {
+        code: CloseCode::Normal,
+        reason: "done resetting".into(),
+    };
+    socket.close(Some(close_frame))
 }
